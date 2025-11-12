548767c1c66bcf59950ff5af0bbece31daf1f4dc
Valentin Raevsky <valentin@compulab.co.il>
2025-11-12 20:06:24 +0200

    Apply kernel patch using git
    Signed-off-by: Valentin Raevsky <valentin@compulab.co.il>

--- scripts/customize.sh
+++ scripts/customize.sh
@@ -4,14 +4,13 @@ set -euo pipefail
 PATCH_DIR="${COMPULAB_DIR}/patches"
 
 # Apply edgeai configurations,
-# botloader and prebuild binaries
+# bootloader and prebuild binaries
 git -C ${L4T_DIR} init 2>/dev/null
 git -C ${L4T_DIR} apply "${PATCH_DIR}/Linux_for_Tegra/"*
 
 # kernel
-cd "${L4T_SRC_DIR}/kernel/kernel-jammy-src"
-patch -p1 < "${PATCH_DIR}/kernel/"*
-make defconfig
+git -C ${L4T_SRC_DIR}/kernel/kernel-jammy-src apply  "${PATCH_DIR}/kernel/"*
+make -C ${L4T_SRC_DIR}/kernel/kernel-jammy-src defconfig
 
 # device tree
 git -C ${L4T_SRC_DIR}/hardware/nvidia/t23x/nv-public apply "${PATCH_DIR}/dts/"*
