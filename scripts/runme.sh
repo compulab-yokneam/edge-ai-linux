c53cbf400b8456064429cc36c9042466aed4766b
vraevsky <valentin@compulab.co.il>
2025-11-12 20:18:50 +0200

    Update runme.sh to use devel branch and create directory

--- scripts/runme.sh
+++ scripts/runme.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -e
 
+EDGE_AI_SDK="edgeai-compulab-bsp"
 REPO_URL="https://github.com/compulab-yokneam/edge-ai-linux"
-SNAPSHOT_URL="$REPO_URL/archive/refs/heads/master.tar.gz"
-curl -fsSL "$SNAPSHOT_URL" | tar -xz
-cd "edge-ai-linux-master/scripts"
+SNAPSHOT_URL="$REPO_URL/archive/refs/heads/devel.tar.gz"
+mkdir ${EDGE_AI_SDK}
+curl -fsSL "$SNAPSHOT_URL" | tar -C ${EDGE_AI_SDK} --strip-components=1 -xz
+cd "${EDGE_AI_SDK}/scripts"
 exec bash ./build.sh all
