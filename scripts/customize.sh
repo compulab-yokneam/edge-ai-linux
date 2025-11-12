3fa4ad2d1093420077fb4af12f478b5beddce3d6
Valentin Raevsky <valentin@compulab.co.il>
2025-11-12 19:58:31 +0200

    Add a new approache of adding the CompuLab configuration
    Signed-off-by: Valentin Raevsky <valentin@compulab.co.il>

--- scripts/customize.sh
+++ scripts/customize.sh
@@ -2,22 +2,16 @@
 set -euo pipefail
 
 PATCH_DIR="${COMPULAB_DIR}/patches"
-FILES_DIR="${COMPULAB_DIR}/files"
 
-cp -v "${FILES_DIR}/conf/*" "${L4T_DIR}/"
+# Apply edgeai configurations,
+# botloader and prebuild binaries
+git -C ${L4T_DIR} init 2>/dev/null
+git -C ${L4T_DIR} apply "${PATCH_DIR}/Linux_for_Tegra/"*
 
 # kernel
 cd "${L4T_SRC_DIR}/kernel/kernel-jammy-src"
 patch -p1 < "${PATCH_DIR}/kernel/"*
 make defconfig
 
-# bootloader
-cd "${L4T_DIR}/bootloader"
-patch -p1 < "${PATCH_DIR}/bl/"*
-
-# Deploy precompiled files
-tar -C "${L4T_DIR}/bootloader" -xvf "${FILES_DIR}/bl/bootloader.tar.bz2"
-
 # device tree
-cd "${L4T_SRC_DIR}/hardware/nvidia/t23x/nv-public"
-git apply "${PATCH_DIR}/dts/"*
+git -C ${L4T_SRC_DIR}/hardware/nvidia/t23x/nv-public apply "${PATCH_DIR}/dts/"*
