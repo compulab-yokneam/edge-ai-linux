3e55b1057cb897944a098f75110073cee905b879
Valentin Raevsky <valentin@compulab.co.il>
2025-11-16 16:00:50 +0200

    Update the customeze.sh
    Signed-off-by: Valentin Raevsky <valentin@compulab.co.il>

--- scripts/customize.sh
+++ scripts/customize.sh
@@ -7,6 +7,23 @@ SRC_TAG="jetson_${VERSION_MAJOR}.${VERSION_MINOR}"
 # CompuLab Resources
 L4T_COMPULAB="https://github.com/compulab-yokneam/compulab-l4t/archive/refs/heads/Linux_for_Tegra.tar.gz"
 
+git_fini() {
+	:
+}
+
+git_init() {
+	set -x
+	git config --global user.name && rc=$? || rc=$?
+	if [ ${rc} -ne 0 ];then
+		git config --global user.email devname@example.com
+		git config --global user.name dev name
+		git_fini() {
+			git config --global --unset user.email
+			git config --global --unset user.name
+		}
+	fi
+}
+
 # root node
 l4t() {
     curl -fsSL ${L4T_COMPULAB} | tar -C ${L4T_DIR} --strip-components=1 -xvz
@@ -25,6 +42,8 @@ device_tree() {
     git -C ${L4T_SRC_DIR}/hardware/nvidia/t23x/nv-public am "${PATCH_DIR}/dts/"*
 }
 
+git_init
 l4t
 kernel
 device_tree
+git_fini
