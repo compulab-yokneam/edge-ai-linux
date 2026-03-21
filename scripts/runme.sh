fa14fc2e92ff12b5e53d5cee059ba02f97fbc48c
Valentin Raevsky <valentin@compulab.co.il>
2026-03-21 12:35:39 +0200

    Update runme.sh: Add EDGE_AI_SDK parameter option
    Signed-off-by: Valentin Raevsky <valentin@compulab.co.il>

--- scripts/runme.sh
+++ scripts/runme.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 
-EDGE_AI_SDK="edgeai-compulab-bsp"
+EDGE_AI_SDK=${EDGE_AI_SDK:-"edgeai-compulab-bsp"}
 REPO_URL="https://github.com/compulab-yokneam/edge-ai-linux"
-SNAPSHOT_URL="$REPO_URL/archive/refs/heads/devel-next.tar.gz"
+SNAPSHOT_URL="${REPO_URL}/archive/refs/heads/devel-next.tar.gz"
 mkdir ${EDGE_AI_SDK}
-curl -fsSL "$SNAPSHOT_URL" | tar -C ${EDGE_AI_SDK} --strip-components=1 -xz
+curl -fsSL "${SNAPSHOT_URL}" | tar -C ${EDGE_AI_SDK} --strip-components=1 -xz
 cd "${EDGE_AI_SDK}/scripts"
 exec bash ./build.sh all
